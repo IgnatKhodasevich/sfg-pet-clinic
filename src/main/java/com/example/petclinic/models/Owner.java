47dc38c49e68a83e3bf2692aaf080b4379b98a71
Ignat <ignat.khodasevich@gmail.com>
2018-09-18 22:46:28 +0300

    Added models

--- src/main/java/com/example/petclinic/models/Owner.java
+++
@@ -1,7 +0,0 @@
-package com.example.petclinic.models;
-
-/**
- * Created by Ignat on 08.08.2018.
- */
-public class Owner extends Person {
-}
--- src/main/java/com/example/petclinic/models/Vet.java
+++
@@ -1,8 +0,0 @@
-package com.example.petclinic.models;
-
-/**
- * Created by Ignat on 08.08.2018.
- */
-public class Vet extends Person{
-
-}
