c89c1949245413725cd42f73e06296388f181eec
Ignat <ignat.khodasevich@gmail.com>
2018-11-13 22:30:13 +0300

    Added modules

--- sfg-pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
+++ sfg-pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
@@ -3,13 +3,6 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.models.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
